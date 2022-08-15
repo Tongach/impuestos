84eb9210d9734758006a4b923f91fc444ecd299b
Tongach <92233165+Tongach@users.noreply.github.com>
2022-08-15 19:39:42 -0300

    Add files via upload

---
+++ IMPUESTOS.js
@@ -0,0 +1,67 @@
+const fnBapro = ()=> {
+	window.open("https://www.bancoprovincia.bancainternet.com.ar/eBanking/login/inicio.htm");
+}
+
+const fnMp = ()=> {
+	window.open("https://www.mercadolibre.com/jms/mla/lgz/msl/login/H4sIAAAAAAAEAzWNQQ7DIBAD_-IzSu4c-xG0JUuCCgEtm5Kqyt8r1PZo2TN-I5U17k5flWHBZ03RR4VBTaShSHZxgUWuMGhR-RcTjQkJZVaWBvseopWXG4ciQxUoNYYBHbq5kEqH_X7BIDbHp7LslFzn-zPyaP_EWmCxqdZm57n3PmUWT0uptJbJlzyRzLgMAjV1KuQfsCoHXx-t-OJLzQAAAA/user");
+
+}
+
+const fnGalicia = ()=> {
+	window.open("https://onlinebanking.bancogalicia.com.ar/login?gclid=Cj0KCQjw3eeXBhD7ARIsAHjssr_ZH7aZi9nKdXM-7hNiWvqxm_MRbQQ4RvhNY0QYlzab0oWNG10TCmYaAgM6EALw_wcB&gclsrc=aw.ds");
+
+}
+
+const fnRds = ()=> {
+	window.open("https://appsb.mardelplata.gob.ar/Consultas/nRecibosSueldo/Account/Login.aspx?ReturnUrl=%2fConsultas%2fnRecibosSueldo%2fVistas%2fUsuario%2fConsultarRecibo.aspx");
+
+}
+
+const fnArba = ()=> {
+	window.open("https://app.arba.gov.ar/LiqPredet/InicioLiquidacion.do?imp=&Frame=NO&origen=WEB&url_volver=https%3A%2F%2Fwww%2Earba%2Egov%2Ear%2F");
+
+}
+
+const fnArm = ()=> {
+	window.open("https://appsvr.mardelplata.gob.ar/consultas/appcontainer/appcontainer.asp?app=ARM_ABLCONSUDEUDA");
+}
+
+const fnEdea = ()=> {
+	window.open("https://www.edeaweb.com.ar/oficina-virtual/ingreso");
+
+}
+
+const fnCamuzzi = ()=> {
+	window.open("https://oficinavirtual.camuzzigas.com.ar/#/landing");
+
+}
+
+const fnFibertel = ()=> {
+	window.open("https://idpsesion.telecom.com.ar/openam/XUI/?realm=/convergente&goto=https://idpsesion.telecom.com.ar:443/openam/oauth2/realms/root/realms/convergente/authorize?response_type%3Dcode%26client_id%3Df70a8442c0f0c7d0d72cd6ee2ac29555%26scope%3Dopenid%2520profile%26redirect_uri%3Dhttps://www.personal.com.ar%26code_challenge%3D-uq3wqmbbeAZOLdrM7kaPAOqg1TMO6ZNTyLzP4tgtN4%26code_challenge_method%3DS256%26_ga%3D2.206242196.666366411.1660601905-374126234.1660601905#login/");
+
+}
+
+const fnOsse = ()=> {
+	window.open("https://app2.osmgp.gov.ar/oficinavirtual/");
+
+}
+
+const fnPatentes = ()=> {
+	window.open("https://www.mardelplata.gob.ar/arm/vehiculos");
+}
+
+document.getElementById("bapro").onclick = fnBapro;
+document.getElementById("baprop").onclick = fnBapro;
+document.getElementById("mp").onclick = fnMp;
+document.getElementById("mpp").onclick = fnMp;
+document.getElementById("galicia").onclick = fnGalicia;
+document.getElementById("galiciap").onclick = fnGalicia;
+document.getElementById("rds").onclick = fnRds;
+document.getElementById("rdsp").onclick = fnRds;
+document.getElementById("arba").onclick = fnArba;
+document.getElementById("arm").onclick = fnArm;
+document.getElementById("edea").onclick = fnEdea
+document.getElementById("camuzzi").onclick = fnCamuzzi;
+document.getElementById("fibertel").onclick = fnFibertel;
+document.getElementById("osse").onclick = fnOsse;
+document.getElementById("patentes").onclick = fnPatentes;
